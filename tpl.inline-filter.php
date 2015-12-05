28ad6a05fd2b9c4af7a55a137a038f7324d8379b
Rudie Dirkx <github@hotblocks.nl>
2015-12-05 22:58:48 +0100

    inline filter+

--- tpl.inline-filter.php
+++ tpl.inline-filter.php
@@ -1,4 +1,19 @@
 <form class="inline-filter">
-	<input name="url_filter" placeholder="Filter URL..." value="<?= html(@$_GET['url_filter']) ?>" class="search" autocomplete="off" onfocus="setTimeout(function(el) { el.classList.add('focus'); el.select(); }, 200, this)" onblur="this.classList.remove('focus')" />
+	<input
+		name="url_filter"
+		placeholder="Filter URL..."
+		value="<?= html(@$_GET['url_filter']) ?>"
+		class="search"
+		autocomplete="off"
+		onfocus="setTimeout(
+			function(el) {
+				el.classList.add('focus');
+				el.select();
+			},
+			'ontouchstart' in document ? 200 : 0,
+			this
+		)"
+		onblur="this.classList.remove('focus')"
+	/>
 	<input type="submit" class="submit" />
 </form>

3039b4942ad9ef810d031df33e54edc2391d91e2
Rudie Dirkx <github@hotblocks.nl>
2016-10-03 23:13:56 +0200

    read title+

--- tpl.header.php
+++ tpl.header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<title><?= $pageTitle ?: 'Later' ?></title>
+<title><?= @$pageTitle ?: 'Later' ?></title>
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta charset="utf-8" />

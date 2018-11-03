93cb6dc22fa332968cca41f44fccc0ee194edfb6
Rudie Dirkx <github@hotblocks.nl>
2018-11-03 16:58:36 +0100

    +theme color

--- tpl.header.php
+++ tpl.header.php
@@ -2,6 +2,7 @@
 <title><?= @$pageTitle ?: 'Later' ?></title>
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="theme-color" content="#333" />
 <meta charset="utf-8" />
 <meta name="referrer" content="no-referrer" />
 <style><?= get_css() ?></style>

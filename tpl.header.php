2ae6b7e0f546c8bd6f5c590087e8eb6ff88d8575
Rudie Dirkx <github@hotblocks.nl>
2019-01-27 15:12:48 +0100

    +web share?

--- tpl.header.php
+++ tpl.header.php
@@ -2,6 +2,7 @@
 <title><?= @$pageTitle ?: 'Later' ?></title>
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="manifest" href="/manifest.json" />
 <meta name="theme-color" content="#333" />
 <meta charset="utf-8" />
 <meta name="referrer" content="no-referrer" />

0d8be8aca6aaa017481abd98b4bc3192583f656e
Rudie Dirkx <github@hotblocks.nl>
2020-12-13 21:39:28 +0100

    +html lang

--- tpl.header.php
+++ tpl.header.php
@@ -1,4 +1,5 @@
 <!doctype html>
+<html lang="en">
 <title><?= @$pageTitle ?: 'Later' ?></title>
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />

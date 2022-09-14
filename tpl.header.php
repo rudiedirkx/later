a90285518fcd3253140f97d33fc82c7a15ae5dfc
Rudie Dirkx <github@hotblocks.nl>
2022-09-14 02:17:53 +0200

    favicon+

--- tpl.header.php
+++ tpl.header.php
@@ -1,7 +1,9 @@
 <!doctype html>
 <html lang="en">
 <title><?= @$pageTitle ?: 'Later' ?></title>
-<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link rel="icon" type="image/png" href="/favicon-128.png" sizes="128x128" />
+<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="manifest" href="/manifest.json" />
 <meta name="theme-color" content="#333" />
